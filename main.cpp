d1d16f3ccb00cc85dd8c96d80d8704d76c50cff5
Максим <KayukMA@example.com>
2020-10-07 14:30:23 +0300

    zagruzka iz seti

--- main.cpp
+++ main.cpp
@@ -107,15 +107,15 @@ int main(int argc, char* argv[])
 {
     if(argc>1)
     {
-        cout<<"argc="<<argc<<endl;
-        for (int i=0; i<argc; i++)
-        {
-            cout<<"argv["<<i<<"]="<<argv[i]<<endl;
-        }
+        CURL* curl =curl_easy_init();
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+         return 0;
     }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto Input=read_input(cin, true);
     const auto bins=make_histogram(Input);
     show_histogram_svg(bins);
-    return 0;
 }
